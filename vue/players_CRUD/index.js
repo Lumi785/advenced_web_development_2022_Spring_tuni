720c17719117238b1efc77798a3fbf6662ef3584
cmjiwa <jinghuan.wang@tuni.fi>
2022-01-25 12:14:08 +0200

    Implement AddPlayerComponent, now can add player and go to the post site can see player is added there

--- vue/players_CRUD/index.js
+++ vue/players_CRUD/index.js
@@ -6,31 +6,44 @@ const AddPlayerComponent = {
   
   data: function () {
     return {
-      value:'',
       player: {
         name: '',
+        id: 0,
         isActive: false 
       },
       reqStatus: ''
     };
   },
- 
-   
 
   methods: {
-    addPlayer(player){
-
+    addPlayer(){
+      const player = {
+        name : `${this.player.name}`,
+        isActive: false
+      };
+      const reqOptions = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(player)
+      };
+      fetch("http://localhost:3001/api/players", reqOptions)
+        .then(response => response.json())
+        .then(data => {
+          this.player = data;
+          console.log("data = ", data)});
     },
-
   },
 
   
-
+// submit.prevent if no use prevent, console.log disappear quickly
   template: 
-  `<form id="submit-player" >
-    <input v-model="value" id="input-player" type="text" placeholder="Enter player name"/>
-    <button id="add-btn" type="submit" @click="addPlayer">Add</button>
-  </form>`
+  `<div>
+  <form @submit.prevent="addPlayer" id="submit-player" >
+    <input v-model="player.name" id="input-player" type="text" placeholder="Enter player name"/>
+    <button  id="add-btn" type="submit" >Add</button>
+  </form>
+  <p>{{player}}</p>
+  </div>`
 
 
 };
