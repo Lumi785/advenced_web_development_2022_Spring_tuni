41458baaec7cf95764a83864fc33465e129c92a1
cmjiwa <jinghuan.wang@tuni.fi>
2022-01-29 13:32:38 +0200

    React-4.2 fetch players succeed

--- react/players_fetch/src/App.js
+++ react/players_fetch/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import { PlayerInfo } from './components/PlayerInfo';
 import { PlayersList } from './components/PlayersList';
@@ -10,7 +11,36 @@ const requestStatus = {
 };
 
 function App () {
-  return null;
+  const [players, setPlayers] = useState([]);
+
+  console.log("hello");
+
+
+  useEffect(() => {
+    fetch("api/players")
+      .then(res=>res.json())
+      .then(data=>{
+        setPlayers(data);
+        
+        console.log("data = ", data);
+        // return data;
+        
+      }).catch(error => {
+        console.log("erros is = ", error);
+      })
+
+  }, [])
+
+
+  return(
+    <>
+    <div>hello rose</div>
+   
+    </>
+    
+  )
+
+
 }
 
 export default App;
