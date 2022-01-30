47ad1aa2fa7881bfb014751d0da8ea0f6ee79940
cmjiwa <jinghuan.wang@tuni.fi>
2022-01-30 16:17:57 +0200

    React4.2 Add url as parameter of onClick in PlayerLink component, now 15/15 tests pass locallt bt npm test cmd.

--- react/players_fetch/src/components/PlayerLink.js
+++ react/players_fetch/src/components/PlayerLink.js
@@ -3,16 +3,29 @@ export const PlayerLink = ({ name, onClick, url, id }) => {
 
   return(
     
-
-    <li role="listitem" id={id} onClick={
+    <li role="listitem" id={id}>
+    <a href={url} role="link" onClick={
       (e) => {
-        onClick();
-        console.log("onlick in p");
+        onClick(url);
+        console.log("onlick url ====== ", url);
         e.preventDefault();
-      }
-      }>
-      <a href={url}>{name}</a>
+      }}
+      >{name}</a>
     </li>
-    
+
   )
 };
+
+
+  
+
+  // this also works, but i understand the course instruction wants the onClick in a element not li element
+  // <li role="listitem" id={id} onClick={
+  //   (e) => {
+  //     onClick(url);
+  //     console.log("onlick in p");
+  //     e.preventDefault();
+  //   }
+  //   }>
+  //   <a role="link" href={url}>{name}</a>
+  // </li>
