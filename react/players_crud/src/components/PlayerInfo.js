e0db3e8a20b7175fed05b803eb938ac39e83ebf9
cmjiwa <jinghuan.wang@tuni.fi>
2022-01-30 16:39:59 +0200

    React4.3 copy four components from react4.2 as instructed by course material

---
+++ react/players_crud/src/components/PlayerInfo.js
@@ -0,0 +1,24 @@
+export const PlayerInfo = ({ player, handleDelete }) => {
+  
+  function c (player){
+    let a;
+    if(player.isActive === false){
+     a = 'not active';
+    } else {
+      a = 'active';
+    }
+    return a;
+  }
+  const aa = c(player);
+ 
+
+  return(
+    <div id="selected-player" onClick={(e)=>{
+      handleDelete(player.id);
+      e.preventDefault();}}>
+      <div className="player-id">{player.id}</div>
+      <div className="player-name">{player.name}</div>
+      <div className="player-status">{aa}</div>
+    </div>
+  )
+};
