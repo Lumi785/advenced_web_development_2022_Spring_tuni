dd167639b7af767c50608c7e377357707f8b18c6
Jaakko Rajala <jaakko.rajala@tuni.fi>
2022-02-04 15:48:07 +0200

    Update redux/redux_thunk/src/redux/reducers/playersReducer.js

--- redux/redux_thunk/src/redux/reducers/playersReducer.js
+++ redux/redux_thunk/src/redux/reducers/playersReducer.js
@@ -5,16 +5,7 @@ import { ADD_PLAYER, REMOVE_PLAYER, SET_PLAYERS } from '../constants';
 const defaultState = [];
 
 const playersReducer = (state = defaultState, action) => {
-	switch (action.type) {
-		case SET_PLAYERS:
-			return action.payload;
-		case ADD_PLAYER:
-			return state.concat(action.payload);
-		case REMOVE_PLAYER:
-			return state.filter((player) => player.id !== action.payload);
-		default:
-			return state;
-	}
+	switch (action.type) {}
 };
 
 export default playersReducer;
