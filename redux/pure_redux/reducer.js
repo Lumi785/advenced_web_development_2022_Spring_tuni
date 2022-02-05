5d9ac40f78162f9e2662927c5aa02db449f85a54
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-05 20:52:25 +0200

    1/16 tests pass, implement reducer.js.

--- redux/pure_redux/reducer.js
+++ redux/pure_redux/reducer.js
@@ -1,20 +1,34 @@
 import * as actions from './actionTypes.js';
 
+let lastId = 0;
 export default (state = [], action) => {
   // TODO: Implement this function
   //throw new Error('Not Implemented');
   switch(action.type){
     case "ADD_PLAYER":
-      return state + action.payload;
+      return [...state, 
+        {
+          id: ++lastId,
+          name: action.payload.name,
+          isActive: false
+        }];
+
     case "REMOVE_PLAYER":
-      return state - action.payload;
+      return state.fileter(player => player.id !== action.payload.id);
+
     case "TOGGLE_PLAYER_STATUS":
-      return state;
+      return state.map(player => player.id === action.payload.id ? true:false);
+
     default:
       return state;
 
   }
 };
+
+
+
+
+
 // export const REMOVE_PLAYER = 'REMOVE_PLAYER';
 
 // export const ADD_PLAYER = 'ADD_PLAYER';
