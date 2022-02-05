843c75ce778f491bd244b95a16e87d5754d37476
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-05 18:12:24 +0200

    Redux5.2 briefly add some codes in reducer.js, ininial commit of Redux-5.2

--- redux/pure_redux/reducer.js
+++ redux/pure_redux/reducer.js
@@ -2,5 +2,20 @@ import * as actions from './actionTypes.js';
 
 export default (state = [], action) => {
   // TODO: Implement this function
-  throw new Error('Not Implemented');
+  //throw new Error('Not Implemented');
+  switch(action.type){
+    case "ADD_PLAYER":
+      return state + action.payload;
+    case "REMOVE_PLAYER":
+      return state - action.payload;
+    case "TOGGLE_PLAYER_STATUS":
+      return state;
+    default:
+      return state;
+
+  }
 };
+// export const REMOVE_PLAYER = 'REMOVE_PLAYER';
+
+// export const ADD_PLAYER = 'ADD_PLAYER';
+// export const TOGGLE_PLAYER_STATUS = 'TOGGLE_PLAYER_STATUS';
