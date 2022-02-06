ea89e56b613a928da12cb76ebcb1082bb1eaec15
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-06 23:23:15 +0200

    Implement PlayersList.js and Player.js, now 12/23 tests pass

--- redux/react_redux/src/components/Player.js
+++ redux/react_redux/src/components/Player.js
@@ -5,8 +5,25 @@ import { removePlayer } from '../redux/actionCreators/playersActions';
 
 export const Player = ({ name, isActive, id }) => {
 	//TODO
+	const dispatch = useDispatch();
+
 	const handleRemove = (id) => {
+		dispatch({
+			type: {removePlayer},
+			payload: id
+		})
+
 	};
+
+
 	//TOCO: complete
-	return ('');
+	return (
+		
+		<div className='player' onClick={handleRemove}>
+			<h3>{id}</h3>
+			<h3>{name}</h3>
+			<h3>{isActive}</h3>
+			<button className='remove-btn'>Remove</button>
+		</div>
+	);
 };
--- redux/react_redux/src/components/PlayersList.js
+++ redux/react_redux/src/components/PlayersList.js
@@ -3,13 +3,23 @@
 import { useSelector} from 'react-redux';
 import { Player } from './Player';
 
-
+const selectPlayers = state => state.players;
 
 //TODO: to be completed
 export const PlayersList = () => {
 
-	return ('');
+	const players = useSelector(selectPlayers);
+	
+	const renderedPlayerItems = players.map(player => {
+		return <Player key={player.id} name={player.name} isActive={player.isActive} />
+	})
+
+	return(
+		<ol id='players-list'>{renderedPlayerItems}</ol>
+
+	)
+
+	
 
 
 };
-
