f9295068beebb95cc2aba6f5393279f258fa66ff
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-06 22:54:14 +0200

    Modify Component/AddPlayer.js

--- redux/react_redux/src/components/AddPlayer.js
+++ redux/react_redux/src/components/AddPlayer.js
@@ -13,14 +13,21 @@ export const AddPlayer = () => {
 	const [name, setName] = useState('');
 	const [isActive, setIsActive] = useState(false);
 	const dispatch = useDispatch();
-	const handleChange = e => setName(e.target.value);
 
-	const handlePlayerSubmit = (e) => {
+	const hanldeToggle = e => setIsActive(!isActive);
+
+	function handlePlayerSubmit(e){
+		console.log("apple");
 		e.preventDefault();
-		const name = e.target.value;
+
+		//Note here should not use below name = e.target.value, becaue then name will be ''
+		// the name now can be read directly from name in state ([name, setName]), same with
+		//isActive
+		// const name = e.target.value;
+		console.log("name = ", name);
 		const player = {name, isActive};
 		dispatch({
-			type: 'addPlayer',
+			type: {addPlayer},
 			payload: player
 		})
 		
@@ -30,11 +37,11 @@ export const AddPlayer = () => {
 	return (
 		<div>
 			<h2>Add player</h2>
-			<form action="" onSubmit={handlePlayerSubmit}>
+			<form onSubmit={handlePlayerSubmit}>
 				<input type="text" name='name' id='name' placeholder='input your name' 
-				value={name} onChange={handleChange}/>
+				value={name} onChange={e=>setName(e.target.value)}/>
 
-				<input type="checkbox" name='active' id='active'/>
+				<input type="checkbox" name='active' id='active' onClick={hanldeToggle}/>
 				<button type='submit'>Add</button>
 			</form>
 
