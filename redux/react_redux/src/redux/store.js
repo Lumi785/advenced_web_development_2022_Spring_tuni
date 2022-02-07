3eae7268014caf133aba4e1c88b091ce87920c9e
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-07 17:36:24 +0200

    Combine reducers even though only has one reducer, but according to teacher request and tutorial, should also combine.

--- redux/react_redux/src/redux/store.js
+++ redux/react_redux/src/redux/store.js
@@ -4,15 +4,17 @@ import { createStore, combineReducers } from 'redux';
 import { playersReducer } from './reducers/playersReducer';
 
 //TODO: store and reducers
-const store = createStore(playersReducer);
-
+const reducers = combineReducers({
+    players: playersReducer
+})
+const store = createStore(reducers);
+       
+//for debugging, because every time the stored change, subscribe will be called automatically
 store.subscribe(()=>{
     console.log("store changed ", store.getState());
 })
-
-
 console.log("store.getState()= ", store.getState());
-export default store;
 
+export default store;
 
 
