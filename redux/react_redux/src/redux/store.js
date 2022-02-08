85e912db4d66c905e11cd909afd637497f1fd694
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-08 19:04:19 +0200

    Now use combined reducers to create store, 21/23 tests pass, the same as before

--- redux/react_redux/src/redux/store.js
+++ redux/react_redux/src/redux/store.js
@@ -4,15 +4,32 @@ import { createStore, combineReducers } from 'redux';
 import { playersReducer } from './reducers/playersReducer';
 
 //TODO: store and reducers
-const store = createStore(playersReducer);
-
-store.subscribe(()=>{
-    console.log("store changed ", store.getState())
-})
 
 
-console.log("store.getState()= ", store.getState());
+// const store = createStore(playersReducer);
+// store.subscribe(()=>{
+//     console.log("store changed ", store.getState())
+// })
+// console.log("store.getState()= ", store.getState());
+// export default store;
+
+
+// below use combine store
+const reducers = combineReducers({
+    playersReducer
+});
+
+const store = createStore(
+    reducers,
+
+    {
+        players: []
+    }
+
+);
+
+
+
 export default store;
-
 
 
