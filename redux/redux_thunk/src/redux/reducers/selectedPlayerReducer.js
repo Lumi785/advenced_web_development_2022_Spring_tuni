7ba6335653e9c7516e852ce80fd4275010a7bbc4
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-09 17:40:00 +0200

    Implement src/redux/reducers/selectedPlayerReducer.js

--- redux/redux_thunk/src/redux/reducers/selectedPlayerReducer.js
+++ redux/redux_thunk/src/redux/reducers/selectedPlayerReducer.js
@@ -2,6 +2,21 @@
 
 import { CLEAR_SELECTED_PLAYER, SET_SELECTED_PLAYER } from '../constants';
 
-const selectedPlayerReducer = (state = {}, action) => {};
+const selectedPlayerReducer = (state = {}, action) => {
+    switch(action.type){
+        case SET_SELECTED_PLAYER:
+            return action.payload;
+
+        case CLEAR_SELECTED_PLAYER:
+            return {};
+        
+        default:
+            return state;
+
+    }
+
+
+
+};
 
 export default selectedPlayerReducer;
