0ed3e41d388c8d65f30bdb3a1da51cdf1f943a3b
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-09 21:58:31 +0200

    4/5 tests pass for src/components/AddPlayer.js

--- redux/redux_thunk/src/components/AddPlayer.js
+++ redux/redux_thunk/src/components/AddPlayer.js
@@ -4,22 +4,25 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { postPlayer } from '../redux/actionCreators/thunks/AddPlayer';
 
-// export const AddPlayer = () => {
-// 	return (null)
-// };
+export const AddPlayer = () => {
 
+	//cannot const [player, setPlater] = useState({XXX})
+  	//should devide the player's properties and each with useState
+	const [name, setName] = useState('');
+	const [isActive, setIsActive] = useState(false);
 
+	const dispatch = useDispatch();
 
-//copied from React/players_crud/src/componentsexport const AddPlayer = ({ handleSubmit }) => {
-  //cannot const [player, setPlater] = useState({XXX})
-  //should devide the player's properties and each with useState
-  const [name, setName] = useState('');
-  const [isActive, setIsActive] = useState(false);
-
+	function handleSubmit(newPlayer){
+		return(dispatch) => {
+			dispatch(postPlayer(newPlayer));
+		}
+	}
+	
   const onSubmit = e => {
     e.preventDefault();
     const player = {name:{name}, isActive:false};
-    handleSubmit({name, isActive});
+    handleSubmit(player);
     setName('');
     
   }
