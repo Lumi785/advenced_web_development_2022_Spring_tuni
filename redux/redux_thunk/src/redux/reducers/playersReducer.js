5d3c83bd8c1e137f462830f81220fdcd9cbf1ab0
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-09 13:02:57 +0200

    Implement  src/redux/reducers/playersReducer.js

--- redux/redux_thunk/src/redux/reducers/playersReducer.js
+++ redux/redux_thunk/src/redux/reducers/playersReducer.js
@@ -2,10 +2,29 @@
 
 import { ADD_PLAYER, REMOVE_PLAYER, SET_PLAYERS } from '../constants';
 
+
 const defaultState = [];
 
 const playersReducer = (state = defaultState, action) => {
-	switch (action.type) {}
+	switch (action.type) {
+		case ADD_PLAYER:
+			console.log("action.payload = ", action.payload);
+
+			//note here not need to set id, because in this excercise, will send post request to backend, where id will be given
+			return [...state, action.payload]
+			
+		case REMOVE_PLAYER:
+		
+			return state.filter(player => player.id !== action.payload);
+			 
+		case SET_PLAYERS:
+			
+			return action.payload
+
+
+		default:
+			return state;
+	}
 };
 
 export default playersReducer;
