c4df4b42ca1a22898bd3cf50f1379eeee950c036
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-21 15:41:25 +0200

    6/12 tests pass src/components/Finder.jsx

--- assignment/frontend/src/components/Finder.jsx
+++ assignment/frontend/src/components/Finder.jsx
@@ -5,6 +5,21 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useParams } from 'react-router-dom';
 
-const Finder = ({ type, findHandler }) => {};
+const Finder = ({ type, findHandler }) => {
+
+    console.log("type = ", type);
+
+
+    return(
+        <>
+        <div data-testid='no-:type-found-component'>
+            :type not found
+        </div>
+        <div data-testid=':type-found-component'>
+            :type
+        </div>
+        </>
+    )
+};
 
 export default Finder;
