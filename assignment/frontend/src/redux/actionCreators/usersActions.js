3377c901eeaba4ade6a45f32a2f4b50fe6385f08
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-21 19:31:08 +0200

    8/8 tests pass for src/redux/actionCreators/usersActions.js

--- assignment/frontend/src/redux/actionCreators/usersActions.js
+++ assignment/frontend/src/redux/actionCreators/usersActions.js
@@ -37,30 +37,21 @@ export const getUser = (userId) => {
 			}
 		}
 
-		console.log("'/api/users'+ userId = ", '/api/users/'+ userId);
-
 		await fetch('/api/users/'+ userId, reqOptions)
-			.then(res => {
-				if(res.ok){
-					return res.json();
+			.then(res => res.json())
+			.then(data => {
+				if(data.error){
+					dispatch({type: NEW_NOTIFICATION, payload: {message: data.error, isSuccess: false}});
+				} else {
+					dispatch({type: GET_USER, payload: data});
 				}
 			})
-			.then(data => {
-				console.log("data === ", data);
-				dispatch({
-					type: GET_USER,
-					payload: data
-				})
-			})
-			.catch(err =>{
-				console.log(err);
-				dispatch({
-					type: NEW_NOTIFICATION,
-					payload: {message: err,  isSuccess: false,}
-				})
-			})
+			.catch(err =>{console.log(err);})
 	}
 };
+
+
+
 /**
  * @description Asynchronous action creator that attempts to get all the users from the backend. If successful, dispatches GET_USERS-type action with users as payload
  * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
@@ -78,44 +69,22 @@ export const getUsers = () => {
 		}
 
 		await fetch('/api/users', reqOptions)
-		.then(res => {
-			if(res.ok){
-				return res.json();
-			}
-			else{
-				console.log("res.statutext = ", res.statusText)
-				console.log("res.json() = ", res.json())
-				console.log("res = ", res.err.message)
-
-			}
-			// else{
-			// 	res.json().then(aa => console.log("aa ==== ", aa))
-			// }
-		})
+		.then(res => res.json())
 		.then(data => {
-			//console.log("data = ", data);
-			dispatch({
-				type: GET_USERS,
-				payload: data
-			})
+			if(data.error){
+				dispatch({type: NEW_NOTIFICATION, payload: {message: data.error, isSuccess: false}});
+			} else {
+				dispatch({type: GET_USERS, payload: data});
+			}
 		})
 		.catch(err => {
 			console.log("err ======= ", err);
-			// dispatch({
-			// 	type: NEW_NOTIFICATION,
-			// 	payload: {message: err, isSuccess: false}
-
-			// })
 		})
 	}
-
-
-
-
-
-
-
 };
+
+
+
 /**
  * @description Asynchronous action creator that updates the given user (if possible). If Successful, dispatches actions in the following order:
  * 1) UPDATE_USER type with user as payload
@@ -141,24 +110,24 @@ export const updateUser = (updatedUser) => {
 		};
 
 		await fetch(url, reqOptions)
-			.then(res => {
-				if(res.ok){
-					return res.json();
-				}
-			})
+			.then(res => res.json())
 			.then(data => {
-				console.log("data from update = ", data);
-				dispatch({type: UPDATE_USER, payload: data});
-				dispatch({type: NEW_NOTIFICATION, payload: {message: userMsg.updateUser, isSuccess: true}})
+				if(data.error){
+					dispatch({type: NEW_NOTIFICATION, payload: {message: data.error, isSuccess: false}});
+				} else {
+					dispatch({type: UPDATE_USER, payload: data});
+					dispatch({type: NEW_NOTIFICATION, payload: {message: userMsg.updateUser, isSuccess: true}})
+				}
 			})
 			.catch(err => {
 				console.log(err);
 				dispatch({type: NEW_NOTIFICATION})
 			})
 	}
-
-	
 };
+
+
+
 /**
  * @description Removes the user (if possible) from the backend. If successful, dispatches actions in following order:
  * 1) REMOVE_USER-type with payload of user.
@@ -181,23 +150,17 @@ export const removeUser = (userId) => {
 		};
 
 		await fetch(url, reqOptions)
-			.then(res => {
-				if (res.ok){
-					return res.json();
+			.then(res => res.json()
+			)
+			.then(data => {
+				if (data.error){
+					dispatch({type: NEW_NOTIFICATION, payload: {message: data.error, isSuccess: false}});
 				} else {
-					dispatch({
-						type: NEW_NOTIFICATION,
-						payload: {message: 'apple', isSuccess: false}
-					})
-					return;
+
+					dispatch({type: REMOVE_USER, payload: data});
+					dispatch({type: NEW_NOTIFICATION, payload: {message: userMsg.delete(data), isSuccess: true}});
 				}
 			})
-			.then(data => {
-				dispatch({type: REMOVE_USER, payload: data});
-				dispatch({type: NEW_NOTIFICATION, payload: {message: userMsg.delete(data), isSuccess: true}});
-			})
 			.catch(err => console.log(err));
-	}
-
-	
+	}	
 };
