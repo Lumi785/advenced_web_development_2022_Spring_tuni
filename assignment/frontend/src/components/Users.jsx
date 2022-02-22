c19b3070df964e8f15f5ae31367fc775863cd218
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-22 22:37:57 +0200

    2/4 tests pass for src/components/Users.jsx, now 78/221 tests pass overall

--- assignment/frontend/src/components/Users.jsx
+++ assignment/frontend/src/components/Users.jsx
@@ -5,6 +5,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../redux/actionCreators/usersActions';
 import User from './User';
 
-const Users = () => {};
+const Users = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        console.log("ursers = ", users);
+        console.log("no users pppppppp")
+        if (users.length === 0){
+            const users = dispatch(getUsers);
+        }
+    }, [])
+
+    const users = useSelector((state) => state.users)
+
+
+
+
+    return(
+        <div data-testid='users-component'>
+            <ul data-testid='users-container'>
+                {users.map(user => {<User providedUser={user} data-testid='user-component'/>})}
+                
+            </ul>
+        </div>
+    )
+};
 
 export default Users;
