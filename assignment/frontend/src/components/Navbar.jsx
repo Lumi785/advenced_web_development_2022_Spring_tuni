fde17a96b4b66e7563fbe253c51fd3f4a8d14e4c
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-25 17:37:09 +0200

    14.14 tests pass for  src/components/Navbar.jsx

--- assignment/frontend/src/components/Navbar.jsx
+++ assignment/frontend/src/components/Navbar.jsx
@@ -15,34 +15,116 @@ const AllLinks = {
  *
  */
 
-
+const selectAuth = state => state.auth;
 const Navbar = () => {
+	console.log("apple ...")
+	const auth = useSelector(selectAuth);
+	console.log("auth from navbar = ", auth);
+
 	const dispatch = useDispatch();
 
 	function handleLogout(e){
 		e.preventDefault();
-		dispatch(logOut);
+		dispatch(logOut());
 
 	}
+
+	switch(auth.role){
+		case 'customer':
+			return (
+
+				<div data-testid='navbar-component'>
+					
+				
+					<Link data-testid='home-link' to='/'>Home  </Link>
+					<Link data-testid='products-link' to='/products'>Product  </Link>
+					<Link data-testid='orders-link' to='/orders'>Orders  </Link>
+					<Link data-testid='cart-link' to='/cart'>Cart  </Link>
+					<Link data-testid='logout-link' to='/logout' onClick={handleLogout}>Log Out</Link>
+					
+				</div>
+			)
+			
+		case 'admin':
+			return (
+				<div data-testid='navbar-component'>
+					<Link data-testid='home-link' to='/'>Home  </Link>
+					<Link data-testid='products-link' to='/products'>Product  </Link>
+					<Link data-testid='orders-link' to='/orders'>Orders  </Link>
+					<Link data-testid='users-link' to='/users'>Users  </Link>
+					<Link data-testid='logout-link' to='/logout' onClick={handleLogout}>Log Out</Link>
+				</div>
+
+			)
+
+		case 'guest':
+			case 'admin':
+				return (
+					<div data-testid='navbar-component'>
+						<Link data-testid='home-link' to='/'>Home  </Link>
+						<Link data-testid='products-link' to='/products'>Products  </Link>
+						<Link data-testid='cart-link' to='/cart'>Cart  </Link>
+						<Link data-testid='login-link' to='/login'>LogIn  </Link>
+						<Link data-testid='register-link' to='/register'>Register  </Link>
+					</div>
+
+				)
+		default:
+			return (
+
+				<div data-testid='navbar-component'>
+					
+				
+					<Link data-testid='home-link' to='/'>Home  </Link>
+					<Link data-testid='products-link' to='/products'>Products  </Link>
+					<Link data-testid='cart-link' to='/cart'>Cart  </Link>
+					<Link data-testid='login-link' to='/login'>LogIn  </Link>
+					<Link data-testid='register-link' to='/register'>Register  </Link>
+					
+				</div>
+			)
+	}
+	
 	
 
-
+		
+	
+	
+}
+/** 
 	return(
 		<div data-testid='navbar-component'>
-			<Link data-testid='home-link' to='/'></Link>
-			<Link data-testid='products-link' to='/products'>Product</Link>
 
-			<Link data-testid='users-link' to='/users'>Users</Link>
-			<Link data-testid='orders-link' to='/orders'>Orders</Link>
-			<Link data-testid='cart-link' to='/cart'>Cart</Link>
-			
-			<Link data-testid='login-link' to='/login'>Log In</Link>
-			<Link data-testid='register-link' to='/register'>Register</Link>
-			<Link data-testid='logout-link' to='/logout' onClick={handleLogout}>Log Out</Link>
+			{role === 'guest' &&
+			<>
+				<Link data-testid='home-link' to='/'>Home  </Link>
+				<Link data-testid='products-link' to='/products'>Product  </Link>
+				<Link data-testid='cart-link' to='/cart'>Cart  </Link>
+				<Link data-testid='login-link' to='/login'>Log In  </Link>
+				<Link data-testid='register-link' to='/register'>Register  </Link>
+			</>}
 
+			{role === 'customer' && 
+			<>
+				<Link data-testid='home-link' to='/'>Home  </Link>
+				<Link data-testid='products-link' to='/products'>Product  </Link>
+				<Link data-testid='orders-link' to='/orders'>Orders  </Link>
+				<Link data-testid='cart-link' to='/cart'>Cart  </Link>
+				<Link data-testid='logout-link' to='/logout' onClick={handleLogout}>Log Out</Link>
+			</>}
+
+			{role === 'admin' &&
+			<>
+				<Link data-testid='home-link' to='/'>Home  </Link>
+				<Link data-testid='products-link' to='/products'>Product  </Link>
+				<Link data-testid='orders-link' to='/orders'>Orders  </Link>
+				<Link data-testid='users-link' to='/users'>Users  </Link>
+				<Link data-testid='logout-link' to='/logout' onClick={handleLogout}>Log Out</Link>
+			</>}
 
 		</div>
 	)
-};
+	**/
+// };
 
 export default Navbar;
