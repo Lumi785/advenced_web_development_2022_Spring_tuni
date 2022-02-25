1317c32b87cce751011479e3daa60c6c76e15402
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-25 10:46:38 +0200

    4/8 tests pass for src/components/Products.jsx

--- assignment/frontend/src/components/Products.jsx
+++ assignment/frontend/src/components/Products.jsx
@@ -6,6 +6,30 @@ import { getProducts } from '../redux/actionCreators/productsActions';
 import Product from './Product';
 import ProductAdder from './ProductAdder';
 
-const Products = () => {};
+const selectProducts = state => state.products;
+
+const Products = () => {
+    const dispatch = useDispatch();
+    const products = useSelector(selectProducts);
+    console.log("products == ", products);
+
+    useEffect(() => {
+        
+        if (products.length === 0){
+            dispatch(getProducts());
+        }
+        
+    }, []);
+
+    return(
+       <ol id='players-list'>
+		
+            {products.map(product => <Product key={product.id} name={product.name} 
+            providedProduct={product} />)}
+               
+		</ol> 
+    )
+
+};
 
 export default Products;
