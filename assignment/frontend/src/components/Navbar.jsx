a1679d52a8837669a30929ecb0a88440f871db5f
cmjiwa <jinghuan.wang@tuni.fi>
2022-02-25 15:36:59 +0200

    10/14 tests pass for src/components/Navbar.jsx

--- assignment/frontend/src/components/Navbar.jsx
+++ assignment/frontend/src/components/Navbar.jsx
@@ -14,6 +14,35 @@ const AllLinks = {
  * @component
  *
  */
-const Navbar = () => {};
+
+
+const Navbar = () => {
+	const dispatch = useDispatch();
+
+	function handleLogout(e){
+		e.preventDefault();
+		dispatch(logOut);
+
+	}
+	
+
+
+	return(
+		<div data-testid='navbar-component'>
+			<Link data-testid='home-link' to='/'></Link>
+			<Link data-testid='products-link' to='/products'>Product</Link>
+
+			<Link data-testid='users-link' to='/users'>Users</Link>
+			<Link data-testid='orders-link' to='/orders'>Orders</Link>
+			<Link data-testid='cart-link' to='/cart'>Cart</Link>
+			
+			<Link data-testid='login-link' to='/login'>Log In</Link>
+			<Link data-testid='register-link' to='/register'>Register</Link>
+			<Link data-testid='logout-link' to='/logout' onClick={handleLogout}>Log Out</Link>
+
+
+		</div>
+	)
+};
 
 export default Navbar;
