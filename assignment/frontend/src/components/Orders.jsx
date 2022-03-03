cdef346239ba286cad3580a42eb9ceed891963aa
cmjiwa <jinghuan.wang@tuni.fi>
2022-03-03 22:59:56 +0200

    4/7 tests pass for src/components/Orders.jsx

--- assignment/frontend/src/components/Orders.jsx
+++ assignment/frontend/src/components/Orders.jsx
@@ -5,6 +5,31 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getOrders } from '../redux/actionCreators/ordersActions';
 import Order from './Order';
 
-const Orders = () => {};
+const seletOrders = state => state.orders;
+const Orders = () => {
+    const dispatch = useDispatch();
+    const orders = useSelector(seletOrders);
+    console.log("orders = ", orders);
+
+    useEffect(() => {
+        if(orders.length === 0){
+            dispatch(getOrders());
+        }
+    }, []);
+
+
+    return(
+
+        <div data-testid='orders-component'>
+            <ul>
+                {orders.map(order => <Order providedOrder={order} key={order.id}></Order>)}
+            </ul>
+        </div>
+
+    )
+
+
+
+};
 
 export default Orders;
