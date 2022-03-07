e460634dd2bb4225e3e9a0e0ddad2453a7f6af68
cmjiwa <jinghuan.wang@tuni.fi>
2022-03-07 14:55:35 +0200

    3/9 tests pass for src/components/Notification.jsx, now 193/69 tests pass overall

--- assignment/frontend/src/components/Notification.jsx
+++ assignment/frontend/src/components/Notification.jsx
@@ -4,6 +4,62 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeNotification } from '../redux/actionCreators/notificationsActions';
 
-const Notification = () => {};
+
+
+
+const selectNotification = state => state.Notification;
+
+const Notification = () => {
+    const dispatch = useDispatch();
+
+    const notification = useSelector(selectNotification);
+    console.log("notification = ", notification);
+    useEffect(() => {
+
+        setTimeout(() => {
+            dispatch(removeNotification(notification));
+        }, 20000);
+    
+    }, [notification])
+
+    
+
+
+    return(
+        
+
+        <div 
+            data-testid='no-notification-component'
+            style={{
+                height: '50px',
+                width: '100%',
+                background: 'green'
+                  
+              }}
+            >
+        </div>
+
+
+
+,
+        
+            
+        <div 
+        data-testid='notification-component'
+        style={{
+            height: '50px',
+            width: '100%',
+            background: 'green'
+              
+          }}
+        >
+            notification.message
+        </div>
+        
+
+
+        
+    )
+};
 
 export default Notification;
