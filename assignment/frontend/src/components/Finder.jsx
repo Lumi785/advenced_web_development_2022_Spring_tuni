e991fb68e156298d060933e3e7f65a58debf71ed
cmjiwa <jinghuan.wang@tuni.fi>
2022-03-09 12:06:08 +0200

    Modify  src/components/Finder.jsx, still 18/18 tests pass, overall 217/262 tests pass now

--- assignment/frontend/src/components/Finder.jsx
+++ assignment/frontend/src/components/Finder.jsx
@@ -10,6 +10,8 @@ const selectProducts = state => state.products;
 const selectOrders = state => state.orders;
 
 const Finder = ({ type, findHandler }) => {
+    const [term, setTerm] = useState('');
+
     const dispatch = useDispatch();
 
     const users = useSelector(selectUsers);
@@ -18,59 +20,54 @@ const Finder = ({ type, findHandler }) => {
 
     const incomeIdObj = useParams();
 
-    let disPlayItem;
 
-    if (type === 'product'){
-        const id = incomeIdObj.productId;
-        disPlayItem = products.find(product => product.id === id);
-
-        useEffect(() => {
+    useEffect(() => {
+        if (type === 'product'){
+            const id = incomeIdObj.productId;
+            const disPlayItem = products.find(product => product.id === id);
             if (!disPlayItem){
                 dispatch(findHandler(id));
-            }
-        }, []);
-    }
-    if (type === 'user'){
-        const id = incomeIdObj.userId;
-        disPlayItem = users.find(user => user.id === id);
-
-        useEffect(() => {
+            };
+            setTerm(disPlayItem);
+        };
+        if (type === 'user'){
+            const id = incomeIdObj.userId;
+            const disPlayItem = users.find(user => user.id === id);
+    
+           
             if (!disPlayItem){
                 dispatch(findHandler(id));
-            }
-        }, []);
-    }
-    if (type === 'order'){
-        const id = incomeIdObj.orderId;
-        disPlayItem = orders.find(order => order.id === id);
-        
-        useEffect(() => {
+            };
+            setTerm(disPlayItem);
+
+            
+        };
+        if (type === 'order'){
+            const id = incomeIdObj.orderId;
+            const disPlayItem = orders.find(order => order.id === id);
+            
             if (!disPlayItem){
                 dispatch(findHandler(id));
-            }
-        }, []);
-    }
+            };
+            setTerm(disPlayItem);
 
-    let itemToUse;
+        };
 
-    if (disPlayItem){
-        itemToUse = disPlayItem;
-    } else {
-        disPlayItem = undefined;
-    }
+    }, []);
+
   
 
     return(
         <>
             {
-                itemToUse === undefined && 
+                term === undefined && 
                 <div data-testid={`no-${type}-found-component`}>
                     {type} not found.
                 </div>
             }
 
             {
-                itemToUse && 
+                term !== undefined && 
                 <div data-testid={`${type}-found-component`}>
                     <Outlet/>
                 </div>
